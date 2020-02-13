fd2a0e209343b7be21529944a7a08182b5df8b5c
Loopii <49533081+Loopii@users.noreply.github.com>
2020-02-13 11:00:08 +0100

    Colors, nav, sample projects

---
+++ js/master.js
@@ -0,0 +1,12 @@
+//Open : Smooth scroll https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+$( document ).ready(function() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+});
+//End
